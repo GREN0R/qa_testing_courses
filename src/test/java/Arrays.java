9b161e2956f59c4df8b450e8535c603d22fa4e70
GRENOR <107197235+GREN0R@users.noreply.github.com>
2022-06-26 20:19:45 +0300

    Add files via upload

---
+++ src/test/java/Arrays.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+public class Arrays {
+    @Test
+    public void Arrays() {
+        int age = 35;
+        int age2 = 40;
+        int age3 = 45;
+        int age4 = 28;
+        int age5 = 30;
+        int[] arr = {35,40,45,28,30};
+        int arr2d[] = {35,40,45,28,30};
+//        System.out.println(arr[0]);
+
+        arr[0] = 25;
+
+        System.out.println(arr[0]);
+        int arrDefault[]= new int[10];
+        System.out.println(arrDefault[3]);
+    }
+}
